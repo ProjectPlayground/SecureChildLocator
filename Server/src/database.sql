3909e7a552eb32399f6c5f943963078535740855
Helena Cruz <helcruz8@gmail.com>
2016-11-23 12:40:02 +0000

    Changes: - primary key for location is now an id because primary keys must be unique - size of session_key increased

--- Server/src/database.sql
+++ Server/src/database.sql
@@ -14,19 +14,20 @@ create table if not exists users
 
 create table if not exists location
 (
+   	location_id		    	int	           auto_increment not null unique,
     location_date       timestamp      not null,
     location            varchar(500)   not null,
-    session_key	        varchar(20)    not null,
-    email				varchar(40)    not null,
-    
-    primary key (session_key)
+    session_key	        varchar(40)    not null,
+    email		        		varchar(40)    not null,
+
+    primary key (location_id)
 );
 
 create table if not exists session_keys
 (
-	session_key	        varchar(20)	  not null,
-    tries		        int			  not null,
-    session_timestamp   timestamp	  not null,
+	  session_key	        varchar(40)	  not null,
+    tries		            int		    	  not null,
+    session_timestamp   timestamp	    not null,
     
     primary key (session_key)
 );
