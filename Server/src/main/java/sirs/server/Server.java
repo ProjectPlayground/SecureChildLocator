697272f584235ca7b342054b1d5db6efbe170818
Helena Cruz <helcruz8@gmail.com>
2016-11-23 16:14:42 +0000

    added a json example to main

--- Server/src/main/java/sirs/server/Server.java
+++ Server/src/main/java/sirs/server/Server.java
@@ -7,6 +7,8 @@ public class Server
 {
     public static void main(String[] args)
     {
+        // Json example
+
         AddUserRequest addUserRequest = new AddUserRequest("h@g.com", "96", "qwerty");
         Gson gson = new Gson();
         String json = gson.toJson(addUserRequest);
