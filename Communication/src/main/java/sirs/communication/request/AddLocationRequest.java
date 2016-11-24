9aa9dbd1f63315c0adaec70ce7c9ebb9fc3d830f
Helena Cruz <helcruz8@gmail.com>
2016-11-24 19:50:03 +0000

    added attribute password to constructor

--- Communication/src/main/java/sirs/communication/request/AddLocationRequest.java
+++ Communication/src/main/java/sirs/communication/request/AddLocationRequest.java
@@ -13,6 +13,7 @@ public class AddLocationRequest extends Request
         this.sessionKey = sessionKey;
         this.email = email;
         this.location = location;
+        this.password = password;
     }
 
     public String getSessionKey()
