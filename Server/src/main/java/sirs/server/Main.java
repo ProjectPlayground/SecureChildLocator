bf8a725958238c1b4a019be9e3770030867153e3
Helena Cruz <helcruz8@gmail.com>
2016-12-04 20:24:06 +0000

    removed comments from main

--- Server/src/main/java/sirs/server/Main.java
+++ Server/src/main/java/sirs/server/Main.java
@@ -1,35 +1,10 @@
 package sirs.server;
 
-import javax.crypto.SecretKey;
-
 public class Main
 {
     public static void main(String[] args) throws Exception
     {
-        /*
-        // Json example
-
-        AddUserRequest addUserRequest = new AddUserRequest("h@g.com", "96", "qwerty");
-        Gson gson = new Gson();
-        String json = gson.toJson(addUserRequest);
-        System.out.println(json);
-        AddUserRequest addUserRequest2 = gson.fromJson(json, AddUserRequest.class);
-        System.out.println(addUserRequest2);
-        */
-
-        // Server example
-
         new Server(9000);
-
-        Cryptography cryptography = new Cryptography();
-
-        SecretKey key = cryptography.generateKey();
-
-        String encrypted = cryptography.encryptAES("I'm awesome!!!", key);
-        String decrypted = cryptography.decryptAES(encrypted, key);
-
-        System.out.println("encrypted: " + encrypted);
-        System.out.println("decrypted: " + decrypted);
     }
 
 }
